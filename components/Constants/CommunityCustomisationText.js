79ba9c73f6b78e94ccb44b230cae8eb7cf1a3bb8
Rahul Mehra <rahulmehra@Rahul-Mehra.local>
2020-03-13 19:41:29 +0530

    removed unwanted and unused folders

--- components/Constants/CommunityCustomisationText.js
+++
@@ -1,6 +0,0 @@
-export const northwesternCustom = {
-  bullet: [
-    `Appear friendly and warm in your profile picture`,
-    `Wear professional attire related to your work`,
-  ],
-}
--- config/constants.js
+++
@@ -1,3 +0,0 @@
-export default {
-  interval: 15, // in minutes
-}
