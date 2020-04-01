6d35224181c2f762775accda9e5a849476f113a5
Rahul Mehra <rahulmehra@Rahul-Mehra.local>
2020-04-01 20:33:14 +0530

    Articles connected system implememnted

--- EP/components/improvements/improvementComp.js
+++ EP/components/improvements/improvementComp.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import _ from 'underscore'
 import ReactHtmlParser from 'react-html-parser'
 import ModalHOC from './../hoc/ModalHOC'
-import { mutuals } from './../../actions/commonActions'
+import { mutuals, log } from './../../actions/commonActions'
 import AsyncImage from './../Loading/asyncImage'
 
 const FocusTrap = require('focus-trap-react')
@@ -16,7 +16,6 @@ class Improvement extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      modalStatus: new Array(100).fill(false),
       key: '',
       cat: '',
       tabIndexes: {
@@ -46,6 +45,10 @@ class Improvement extends Component {
         [this.props.appUrls.modulation]: 124,
         [this.props.appUrls.videosummary]: 134,
       },
+      currentIndex: null,
+      currentItem: null,
+      articles: [],
+      modalOpen: false,
     }
   }
 
@@ -54,6 +57,16 @@ class Improvement extends Component {
     var cat = loc.split('/')[5]
     this.setState({ category: cat })
     this.tabIndexDeactivate()
+    this.modifyData(cat)
+  }
+
+  modifyData(cat) {
+    let temp = this.props.improveArticles.filter((item, index) => {
+      if (cat === item.category) return true
+      else return false
+    })
+
+    this.setState({ articles: temp })
   }
 
   UNSAFE_componentWillReceiveProps(props) {
@@ -62,21 +75,29 @@ class Improvement extends Component {
       var cat = loc.split('/')[4]
       this.setState({ category: cat })
       this.tabIndexDeactivate()
+      this.modifyData(cat)
     }
   }
 
-  modalToggler(index, title) {
+  modalOpener(index, item) {
     if (this.refs.floatingPlayer) this.refs.floatingPlayer.pause()
-    let status
-    this.state.modalStatus[index] ? (status = '_closed') : (status = '_opened')
-
     mutuals.socketTracking({
       event_type: 'click',
-      event_description: 'Article ' + title + status,
+      event_description: 'Article' + item.title + '_opened',
     })
 
-    this.state.modalStatus[index] = !this.state.modalStatus[index]
-    this.setState({ modalStatus: this.state.modalStatus })
+    this.setState({ currentIndex: index, currentItem: item, modalOpen: true })
+  }
+
+  modalClose(index, item) {
+    mutuals.socketTracking({
+      event_type: 'click',
+      event_description: 'Article' + item.title + '_closed',
+    })
+
+    this.setState({ modalOpen: false }, () => {
+      this.setState({ currentIndex: null, currentItem: null })
+    })
   }
 
   tabIndexActivate() {
@@ -121,12 +142,54 @@ class Improvement extends Component {
     })
   }
 
-  render() {
-    let { tabIndexesMain, category, tabIndexes } = this.state
-    let compLoader = this.props.common.compLoader
-    let improveArticles = this.props.improveArticles
+  forward() {
+    if (this.state.currentIndex < this.state.articles.length - 1) {
+      this.setState(
+        {
+          currentIndex: this.state.currentIndex + 1,
+        },
+        () => {
+          this.setState({
+            currentItem: this.state.articles[this.state.currentIndex],
+          })
+        }
+      )
+    } else {
+      this.setState({ currentIndex: 0, currentItem: this.state.articles[0] })
+    }
+  }
 
-    if (improveArticles.length <= 0) {
+  previous() {
+    if (this.state.currentIndex > 0) {
+      this.setState(
+        {
+          currentIndex: this.state.currentIndex - 1,
+        },
+        () => {
+          this.setState({
+            currentItem: this.state.articles[this.state.currentIndex],
+          })
+        }
+      )
+    } else {
+      this.setState(
+        {
+          currentIndex: this.state.articles.length - 1,
+        },
+        () => {
+          this.setState({
+            currentItem: this.state.articles[this.state.currentIndex],
+          })
+        }
+      )
+    }
+  }
+
+  render() {
+    let { tabIndexesMain, category, tabIndexes, articles } = this.state
+    let compLoader = this.props.common.compLoader
+
+    if (articles.length <= 0) {
       return (
         <div className="cardStyle clearfix bg-white p-6 mt-4">
           <div className="clearfix loaderWrap">
@@ -158,8 +221,8 @@ class Improvement extends Component {
 
           <div className="hr mt-4" />
 
-          {improveArticles.map((item, index) => {
-            return category === item.category ? (
+          {this.state.articles.map((item, index) => {
+            return (
               <div
                 className="improv"
                 key={index}
@@ -167,13 +230,13 @@ class Improvement extends Component {
                 aria-label={`${item.title}. click to read more.`}
                 onKeyPress={e => {
                   if (e.key === 'Enter') {
-                    this.modalToggler(index, item.title)
+                    this.modalOpener(index, item)
                   }
                 }}>
                 <div
                   className="clearfix relative mt-6 cursor-pointer"
                   onClick={() => {
-                    this.modalToggler(index, item.title)
+                    this.modalOpener(index, item)
                   }}>
                   <span className="float-left w-1/5 text-center">
                     <AsyncImage
@@ -190,19 +253,20 @@ class Improvement extends Component {
                     <div className="hint elipsis">{item.summary}</div>
                   </div>
                 </div>
-
-                {this.state.modalStatus[index] ? (
-                  // <FocusTrap>
-                  <Modal
-                    item={item}
-                    index={index}
-                    modalToggler={this.modalToggler.bind(this)}
-                  />
-                ) : // </FocusTrap>
-                null}
               </div>
-            ) : null
+            )
           })}
+
+          {this.state.modalOpen ? (
+            <Modal
+              item={this.state.currentItem}
+              index={this.state.currentIndex}
+              modalClose={this.modalClose.bind(this)}
+              forward={this.forward.bind(this)}
+              previous={this.previous.bind(this)}
+              articles={this.state.articles}
+            />
+          ) : null}
         </div>
       )
     }
@@ -210,84 +274,112 @@ class Improvement extends Component {
 }
 
 const mapStateToProps = state => {
-  let combinedRes = null
-
-  if (!_.isEmpty(state.results)) {
-    if (state.results.eyeResults) {
-      combinedRes = state.results.eyeResults
-    }
-  }
-
   return {
-    combinedRes: combinedRes,
     common: state.commonStuff,
-    concatData: !_.isEmpty(state.concatenateResults)
-      ? state.concatenateResults
-      : null,
-    gentleData: !_.isEmpty(state.gentleResults) ? state.gentleResults : null,
-    punctData: !_.isEmpty(state.punctuatorResults)
-      ? state.punctuatorResults
-      : null,
     improveArticles: state.improveArticles,
   }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {}
 }
 
 export class ModalTemplate extends Component {
   constructor(props) {
     super(props)
-    this.attachEscEvent()
+    this.attachEscEvent = this.attachEscEvent.bind(this)
   }
 
-  attachEscEvent() {
-    document.onkeydown = e => {
-      if (e.key === 'Escape')
-        this.props.modalToggler(this.props.index, this.props.item.title)
-    }
+  componentDidMount() {
+    window.addEventListener('keydown', this.attachEscEvent)
+  }
+
+  attachEscEvent(e) {
+    if (e.key === 'Escape')
+      this.props.modalClose(this.props.index, this.props.item)
   }
 
   componentWillUnmount() {
-    document.onkeydown = null
+    window.removeEventListener('keydown', this.attachEscEvent)
   }
 
   render() {
     let { props } = this
     return (
-      <div className="epModalCover">
-        <div className="epModal">
-          <div className="text-24-bold pl-10 pt-10 pb-8">Suggested Reads</div>
+      <FocusTrap>
+        <div className="epModalCover">
+          <div className="epModal">
+            <div className="text-24-bold pl-10 pt-10 pb-8">Suggested Reads</div>
 
-          <button
-            className="epModalClose"
-            style={{ zIndex: 1 }}
-            onClick={() => {
-              props.modalToggler(props.index, props.item.title)
-            }}
-            tabIndex="1"
-            aria-label={'improvement modal close button'}>
-            <span className="ep-icon-close"></span>
-          </button>
+            <button
+              className="epModalClose"
+              style={{ zIndex: 1 }}
+              onClick={() => {
+                props.modalClose(props.index, props.item)
+              }}
+              tabIndex={0}
+              aria-label={'improvement modal close button'}>
+              <span className="ep-icon-close"></span>
+            </button>
 
-          <AsyncImage
-            src={props.item.images}
-            width={768}
-            height={256}
-            loaderSize={1.2}
-            alt={props.item.title}
-            style={{ borderRadius: 0 }}
-          />
+            <AsyncImage
+              src={props.item.images}
+              width={768}
+              height={256}
+              loaderSize={1.2}
+              alt={props.item.title}
+              style={{ borderRadius: 0 }}
+            />
 
-          <div className="epModalContent">
-            <div className="subHead">{props.item.title}</div>
-            <div className="mt-6">
-              {ReactHtmlParser(props.item.description)}
+            <div
+              className="epModalContent overflow-auto"
+              style={{ height: 360 }}>
+              <div className="subHead">{props.item.title}</div>
+              <div className="mt-6">
+                {ReactHtmlParser(props.item.description)}
+              </div>
+            </div>
+
+            <div className="py-6 text-center">
+              <span>
+                <button
+                  onClick={() => {
+                    this.props.previous()
+                  }}
+                  className="traverse brand-blue-color"
+                  tabIndex={0}
+                  aria-label={'article previous move button'}>
+                  <span
+                    className="ep-icon-expand-left"
+                    style={{
+                      '-webkit-text-stroke': '1px',
+                      'vertical-align': -2,
+                    }}
+                  />
+                  <span className="ml-6">Prev</span>
+                </button>
+
+                <span className="para grey-color mx-12">{`${this.props.index +
+                  1}/${this.props.articles.length}`}</span>
+
+                <button
+                  onClick={() => {
+                    this.props.forward()
+                  }}
+                  className="traverse brand-blue-color"
+                  tabIndex={0}
+                  aria-label={'article forward move button'}>
+                  <span>Next</span>
+
+                  <span
+                    className="ep-icon-expand-right ml-6 align-middle"
+                    style={{
+                      '-webkit-text-stroke': '1px',
+                      'vertical-align': -2,
+                    }}
+                  />
+                </button>
+              </span>
             </div>
           </div>
         </div>
-      </div>
+      </FocusTrap>
     )
   }
 }
@@ -296,5 +388,5 @@ const Modal = ModalHOC(ModalTemplate)
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  {}
 )(Improvement)
