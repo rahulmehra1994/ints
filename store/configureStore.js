72a9fbd567912071b4c555ce03f9e60df9fee33a
Rahul Mehra <rahulmehra@Rahul-Mehra.local>
2020-04-17 10:14:50 +0530

    1. removed unwanted earlier store file 2. changes reverted back in configureStore file

--- store/configureStore.js
+++ store/configureStore.js
@@ -15,8 +15,7 @@ let middleware
 if (process.env.APP_ENV === 'live') {
   middleware = applyMiddleware(thunk, routerMiddleWare)
 } else {
-  // middleware = applyMiddleware(thunk, routerMiddleWare, logger)
-  middleware = applyMiddleware(thunk, routerMiddleWare)
+  middleware = applyMiddleware(thunk, routerMiddleWare, logger)
 }
 
 function configureStore() {
