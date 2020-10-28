5e3b25f3790aeb8a14e6265cda91a98be6d6c68c
Rahul <rhlmehra1994@gmail.com>
2020-10-28 12:08:24 +0530

    1. back button press on interview handled by confirm dialog 2. now on back button press on interview will refresh the calibration page to kill the processes on interview

--- EP/components/containers/Interview.js
+++ EP/components/containers/Interview.js
@@ -34,6 +34,7 @@ import {
   sendNoOfVideoClips,
   uploadVideoAPI,
   submitTranscriptApi,
+  changeInterviewToSuccess,
 } from './../../actions/interviewActions'
 import {
   highContrast,
@@ -167,6 +168,29 @@ class Interview extends Component {
       event_type: 'mount',
     })
     this.checkCameFromCalibration()
+    this.setWasOnInterview()
+  }
+
+  setWasOnInterview() {
+    window.addEventListener('popstate', this.checkBackButtonPressed, false) // page is refreshing so no need to remove this event listener
+  }
+
+  checkBackButtonPressed = () => {
+    changeInterviewToSuccess()
+    // The popstate event is fired each time when the current history entry changes.
+    var r = confirm('You pressed a Back button! Are you sure?')
+
+    if (r == true) {
+      // Call Back button programmatically as per user confirmation.
+      //  history.back();
+      // Uncomment below line to redirect to the previous page instead.
+      window.location = document.referrer // Note: IE11 is not supporting this.
+    } else {
+      // Stay on the current page.
+      history.pushState(null, null, window.location.pathname)
+    }
+
+    history.pushState(null, null, window.location.pathname)
   }
 
   adminsFunctionalityActivation() {
